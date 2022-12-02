2dd2ded3915e75f03d578beb716009494028f432
Eron Alves <eron.alves@rocketmail.com>
2022-12-02 01:46:57 -0300

    Make a fiber implementation on top of server listening

--- src/Server.rb
+++ src/Server.rb
@@ -2,15 +2,35 @@ require 'socket'
 
 class App
 
-    @@server
+  @@f = []
 
-    def use(port)
-        puts "Listening on " + port.to_s
-        server = TCPServer.new port
-        loop do 
-            client = server.accept
-            client.puts "Hello world!"
-            client.close
-        end
+  @@server
+
+  def use(port)
+    puts "Listening on " + port.to_s
+    
+    fiber = Fiber.new do
+      server = TCPServer.new port
+      loop do 
+        client = server.accept
+        client.puts "Hello world!"
+        client.close
+        Fiber.yield
+      end
     end
+
+    @@f.push(fiber)
+    
+    i = 0
+
+    p(@@f.length())
+    while i < @@f.length()
+      @@f.at(i).resume()
+      i += 1
+      if i == @@f.length
+        i = 0
+      end
+    end
+
+  end
 end
