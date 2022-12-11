4ffd3f53ae290b695829e8e48a854955562034aa
Eron Alves <eron.alves@rocketmail.com>
2022-12-11 12:00:11 -0300

    FEATURE: parse headers

--- lib/girassol/http/request.rb
+++ lib/girassol/http/request.rb
@@ -3,18 +3,28 @@
 module Girassol
     module HTTP
         class Request
-            def initialize
+            attr_reader :method, :route , :headers
 
+            def initialize
+                @headers = Hash.new
             end
+
             def parse(client)
                 first_line = client.gets.split(" ")
                 if first_line[2] != "HTTP/1.1"
                     # TODO: return 400 on the request
                     return
                 end
+                @method = first_line[0]
+                @route = first_line[1]
 
-
-
+                while true
+                    header = client.gets
+                    if header == "/r/n"
+                        break
+                    end
+                    header = header.split ":"
+                    headers.store(header[0], header[1])
             end
         end
     end
