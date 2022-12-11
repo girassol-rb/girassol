629e8dfc91474137d29d370b713aa51b6170e444
Eron Alves <eron.alves@rocketmail.com>
2022-12-11 11:55:16 -0300

    FEATURE: parsed first line

--- lib/girassol/http/request.rb
+++ lib/girassol/http/request.rb
@@ -3,6 +3,19 @@
 module Girassol
     module HTTP
         class Request
+            def initialize
+
+            end
+            def parse(client)
+                first_line = client.gets.split(" ")
+                if first_line[2] != "HTTP/1.1"
+                    # TODO: return 400 on the request
+                    return
+                end
+
+
+
+            end
         end
     end
 end
